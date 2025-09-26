4e823ed8ef13d7ddc66f83b27c70a3140cdaadf7
Akee Marcus <Akeemarcus@gmail.com>
2025-09-25 21:49:17 -0400

    Added MongoDBConnector constructor with connection string

--- MongoDBConnector/MongoDBConnector.cs
+++ MongoDBConnector/MongoDBConnector.cs
@@ -1,7 +1,14 @@
-﻿namespace MongoDBConnector
+﻿using MongoDB.Driver;
+
+namespace MongoDBConnector
 {
     public class MongoDBConnector
     {
+        private readonly IMongoClient _client;
 
+        public MongoDBConnector(string connectionString)
+        {
+            _client = new MongoClient(connectionString);
+        }
     }
 }
