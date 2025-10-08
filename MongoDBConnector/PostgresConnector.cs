b60204abc6812617767a067e43b7cfde8a1c45ca
Akee Marcus <Akeemarcus@gmail.com>
2025-10-08 16:10:41 -0400

    PostgresConnector Interface

---
+++ MongoDBConnector/PostgresConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace MongoDBConnector
+{
+    public class PostgresConnector : IDBConnector
+    {
+        private readonly string _connectionString;
+
+        public PostgresConnector(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("connectionString must not be null or empty.", nameof(connectionString));
+
+            _connectionString = connectionString;
+        }
+
+        public async Task<bool> PingAsync()
+        {
+            try
+            {
+                await using var connection = new NpgsqlConnection(_connectionString);
+                await connection.OpenAsync();
+                await using var command = new NpgsqlCommand("SELECT 1", connection);
+                await command.ExecuteScalarAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
